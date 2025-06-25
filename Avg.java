bfd52048a3d63b4abfdec94861668eba9d4d5d85
om05jadhav <160996956+om05jadhav@users.noreply.github.com>
2025-06-25 14:50:21 +0530

    Add files via upload

---
+++ Avg.java
@@ -0,0 +1,19 @@
+package Methods;
+import java.util.Scanner;
+public class Avg {
+    public static int avgof3(int a, int b, int c){
+        int avg = (a+ b+ c) /3;
+        return avg;
+
+    }
+
+    public static void main( String args[]){
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int avg  = avgof3(a, b, c);
+        System.out.println(avg);
+    }
+}
